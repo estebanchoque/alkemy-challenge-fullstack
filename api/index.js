d2d9d8748148d3079c6b7907b2056c17bd4845c1
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-12-17 19:17:51 -0300

    refactor(backend): Modify PORT from 3000 to 3001

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const server = require("./src/app");
 const { conn } = require("./src/db");
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => console.log(`%s listening at ${PORT}`));
