a54ac0c7d1324b2ed4dbc12e96faeade1650b0f2
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-12-17 17:48:35 -0300

    feat(backend): Add body-parser middleware

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
 const routes = require("./routes/index");
@@ -11,7 +12,8 @@ const server = express();
 server.name = "API";
 
 server.use(cors());
-server.use(express.json());
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(morgan("dev"));
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
