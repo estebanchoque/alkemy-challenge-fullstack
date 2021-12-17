e666e0f5a19785b93cf76005c940354dd9a3a74a
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-12-17 17:23:29 -0300

    feat(backend): Add database models folder

---
+++ api/src/models/User.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define("user", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+    },
+  });
+};
