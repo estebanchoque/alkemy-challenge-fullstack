66398677babc697a2df9d5d1bd98e306b37f9792
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-12-26 20:08:08 -0300

    feat(api/errors): add custom error generator

---
+++ api/src/errors/custom-error.js
@@ -0,0 +1,15 @@
+class CustomAPIError extends Error {
+  constructor(statusCode, message) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+}
+
+const createCustomError = (statusCode, message) => {
+  return new CustomAPIError(statusCode, message);
+};
+
+module.exports = {
+  CustomAPIError,
+  createCustomError,
+};
