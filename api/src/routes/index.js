68e0aa0d347d4e70f05a02b706e84ba035cb6850
estebanchoque <choqueestebanjoaquin@gmail.com>
2021-12-26 20:54:00 -0300

    feat(api/routes): add index file to export routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,3 +1,9 @@
 const router = require("express").Router();
 
+const operationsRouter = require("./operations");
+const categoriesRouter = require("./categories");
+
+router.use("/operations", operationsRouter);
+router.use("/categories", categoriesRouter);
+
 module.exports = router;
